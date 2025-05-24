d55eacbcaef568298b46fde77a25de3f402424d9
MT Dev1 <MT-DEV>
2025-05-24 16:00:16 +0530

    completed

--- Employee_management_system/Program.cs
+++ Employee_management_system/Program.cs
@@ -1,87 +1,3 @@
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.UI.Services;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Diagnostics;
-//using Employee_Management_System.Data;
-//using Employee_Management_System.Services;
-//using Employee_management_system.Models.Entities;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container
-//builder.Services.AddControllersWithViews();
-
-//builder.Services.AddTransient<IEmailSender, EmailSender>();
-
-//// Configure EF Core with SQL Server
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
-//    options.ConfigureWarnings(warnings =>
-//        warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
-//});
-
-
-
-//// Configure Identity
-//builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-//{
-//    // Password policy
-//    options.Password.RequireDigit = true;
-//    options.Password.RequireLowercase = true;
-//    options.Password.RequireNonAlphanumeric = true;
-//    options.Password.RequireUppercase = true;
-//    options.Password.RequiredLength = 6;
-//    options.Password.RequiredUniqueChars = 1;
-
-//    // Lockout
-//    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-//    options.Lockout.MaxFailedAccessAttempts = 5;
-//    options.Lockout.AllowedForNewUsers = true;
-
-//    // User
-//    options.User.RequireUniqueEmail = true;
-
-//    // Email confirmation
-//    options.SignIn.RequireConfirmedEmail = true;
-//})
-//.AddEntityFrameworkStores<ApplicationDbContext>()
-//.AddDefaultTokenProviders();
-
-//// Configure Authentication Cookie
-//builder.Services.ConfigureApplicationCookie(options =>
-//{
-//    options.Cookie.HttpOnly = true;
-//    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-//    options.LoginPath = "/Account/Login";
-//    options.AccessDeniedPath = "/Account/AccessDenied";
-//    options.SlidingExpiration = true;
-//});
-
-//var app = builder.Build();
-
-//// Middleware pipeline
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthentication();
-//app.UseAuthorization();
-
-//// Default route
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Account}/{action=Login}/{id?}");
-
-//app.Run();
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -155,11 +71,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Authentication and Authorization
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Default route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}");
